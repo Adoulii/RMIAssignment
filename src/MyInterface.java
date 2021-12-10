9a5714d59b8cb709d0cf3bb1f261f3d4bb52a071
Omar Adouli <47005456+Adoulii@users.noreply.github.com>
2021-12-10 17:56:39 +0100

    Update MyInterface.java

--- src/MyInterface.java
+++ src/MyInterface.java
@@ -2,5 +2,8 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface MyInterface extends Remote {
-
+    public String reverse(String s) throws RemoteException;
+    public String min(String s) throws RemoteException;
+    public String caseChanger(String s) throws RemoteException;
+    
 }
